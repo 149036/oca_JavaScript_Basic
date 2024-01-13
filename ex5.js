5de996d09211fc013364d69c8807b1bfbb8440c6
149036 <sqrt2c@gmail.com>
2024-01-13 13:17:31 +0900

    fix

--- ex5.js
+++ ex5.js
@@ -1,4 +1,11 @@
-array1 = [1,2,3];
-array2 = ["abc","def","ghi"];
+array1 = [1, 2, 3];
+array1.pop();
+
+array2 = ["abc", "def", "ghi"];
+array2.pop();
+
 array3 = [1, "a", 42];
+array3.pop();
+
 array4 = [...array1, ...array2, ...array3];
+array4;
