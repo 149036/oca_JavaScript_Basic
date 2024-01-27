c87583e48916e212b1d4387162e3e5d368adc0fc
149036 <sqrt2c@gmail.com>
2024-01-27 16:03:14 +0900

    hello2

--- try_typescript/src/ex1.ts
+++ try_typescript/src/ex1.ts
@@ -2,5 +2,10 @@ function hello(): void {
   const message: string = "hello";
   console.log(message);
 }
-
 hello();
+
+
+const hello2: () => void = (): void => {
+  console.log("hello2");
+};
+hello2();
