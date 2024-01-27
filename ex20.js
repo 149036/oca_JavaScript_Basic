ffb88a633934c0a3b0a2d0b8d7c21d3ddef24e54
149036 <sqrt2c@gmail.com>
2024-01-27 13:28:33 +0900

    ex20

---
+++ ex20.js
@@ -0,0 +1,15 @@
+const any_success = Promise.any([
+  Promise.resolve("success"),
+  Promise.resolve(new Error("エラー")),
+  Promise.resolve(true),
+]);
+
+const all_fail = Promise.any([
+  Promise.reject(new Error("エラー")),
+  Promise.reject(new Error("エラー")),
+  Promise.reject(new Error("エラー")),
+]);
+
+all_fail;
+
+any_success;
